015209626fbd86e6422e432d117989a1779e5951
Becca White <beccawhite@Beccas-MBP.home>
2021-04-02 15:13:35 -0700

    initial set up

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_owner = models.BooleanField(default=True)
+    is_sitter = models.BooleanField(default=False)
+    pets = models.CharField(max_length=100)
+    profile_picture = models.CharField(max_length=100)
+    sit_count = models.IntegerField()
+    pet_experience = models.CharField(max_length=100)
+    rating = models.CharField(max_length=100)
+    profile_picture = models.CharField(max_length=100)
+
