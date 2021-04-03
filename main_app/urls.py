722929a1dbd3fc5582583b914a19018b8fcf2812
Nina Kim <neenerskim@gmail.com>
2021-04-03 13:23:47 -0700

    finalize models and user authentication

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     # path('posts/', views.paths, name='posts'),
-    path('accounts/signup/', views.signup, name="signup"),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('accounts/signup/owner', views.owners_signup, name='owners_signup'),
+    path('accounts/signup/sitter', views.sitters_signup, name='sitters_signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from django.http import HttpResponse
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+from .models import Owner, Sitter, Pet, Post, MyUser
 
 # Define the home view
 def home(request):
@@ -15,6 +14,9 @@ def about(request):
 
 
 def signup(request):
+  return render(request, 'registration/signup.html')
+
+def owners_signup(request):
   error_message = ''
   if request.method == 'POST':
     # This is how to create a 'user' form object
@@ -31,5 +33,24 @@ def signup(request):
   # A bad POST or a GET request, so render signup.html with an empty form
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
-  return render(request, 'registration/signup.html', context)
+  return render(request, 'registration/owners_signup.html', context)
 
+
+def sitters_signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/sitters_signup.html', context)
