ec931f4ac4daa0051269128afbe2aca0fbd19a3f
Nina Kim <neenerskim@gmail.com>
2021-04-03 21:13:38 -0700

    redirect user to profile page aka owner details or sitter details

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     # path('posts/', views.paths, name='posts'),
     path('accounts/signup/', views.signup, name='signup'),
     path('owners/', views.owners_index, name='owners_index'),
+    path('owners/<int:owner_id>/', views.owners_detail, name='detail'),
     path('owners/create/', views.OwnerCreate.as_view(), name='owners_create'),
     path('sitters/', views.sitters_index, name='sitters_index'),
+    path('sitters/<int:sitter_id>/', views.sitters_detail, name='detail'),
     path('sitters/create/', views.SitterCreate.as_view(), name='sitters_create'),
 ]
