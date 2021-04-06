be8f7b948d2ba64c965abb7de7b369ec81d18734
atusan <atunaseri@gmail.com>
2021-04-06 08:15:46 -0700

    posts list and add post created

--- main_app/views.py
+++ main_app/views.py
@@ -161,7 +161,17 @@ def add_photo(request, sitter_id):
             print('An error occurred uploading file to S3')
     return redirect('sitters_detail', sitter_id=sitter_id)
 
+
 #creating post by owner
+def posts_create(request, owner_id):
+      owner = Owner.objects.get(id=owner_id)
+      post_form = PostingForm()
+      return render(request, 'owners/post_form.html', {
+        'owner': owner,
+        'post_form': post_form
+      })
+
+#adding post by owner
 def add_posting(request, owner_id):
       	
   form = PostingForm(request.POST)
@@ -171,8 +181,9 @@ def add_posting(request, owner_id):
     new_posting.owner_id = owner_id
     new_posting.save()
   # return redirect('detail', owner_id=owner_id)
-  return redirect('posts')
+  return redirect('index')
 
+  
 # pots list
 def posts_index(request):
       posts = Post.objects.all()
